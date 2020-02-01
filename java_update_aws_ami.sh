2c802355367ac2c66a127e562eb4343ca79f717f
coskunbalta <9549361+coskunbalta@users.noreply.github.com>
2020-02-01 07:30:06 +0000

    Initial Commit
    Code to update java from java 1.7 to java 1.8 on amazon linux ami. Enables Jenkins process to start.

---
+++ java_update_aws_ami.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+sudo yum update
+sudo yum upgrade
+sudo yum install -y java-1.8.0-openjdk.x86_64
+sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
+sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac
+
+#this will fail if java has already been uninstalled via yum. 
+sudo yum remove java-1.7
