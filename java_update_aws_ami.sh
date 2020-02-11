92fc3be26e157e40b3ec0145989718b15ec33b7d
coskunbalta <9549361+coskunbalta@users.noreply.github.com>
2020-02-11 11:49:33 +0000

    Fixing syntax errors and automate yum update

--- java_update_aws_ami.sh
+++ java_update_aws_ami.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
-sudo yum update
-sudo yum upgrade
-sudo yum install -y java-1.8.0-openjdk.x86_64
-sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java
-sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac
+sudo yum update -y;
+sudo yum upgrade -y;
+sudo yum install -y java-1.8.0-openjdk.x86_64;
+sudo /usr/sbin/alternatives --set java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java;
+sudo /usr/sbin/alternatives --set javac /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/javac;
 
 #this will fail if java has already been uninstalled via yum. 
-sudo yum remove java-1.7
+sudo yum remove java-1.7;
